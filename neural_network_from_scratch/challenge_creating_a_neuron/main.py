2cfff9dbfec07adcf31e3f28d16a378ce457be3f
ga771065-shih <>
2026-09-21 13:16:47 +0000

    Challenge: Creating a Neuron

--- neural_network_from_scratch/challenge_creating_a_neuron/main.py
+++ neural_network_from_scratch/challenge_creating_a_neuron/main.py
@@ -9,14 +9,15 @@ def sigmoid(z):
 class Neuron:
     def __init__(self, n_inputs):
         # 1. Initialize weights and bias with random values
-        self.weights = ___
-        self.bias = ___
+        self.weights = np.random.uniform(-1, 1, size=n_inputs)
+
+        self.bias = np.random.uniform(-1, 1)
 
     def activate(self, inputs):
         # 2. Compute the weighted sum using dot product and add bias
-        input_sum_with_bias = ___
+        input_sum_with_bias = np.dot(inputs, self.weights) + self.bias
         # 3. Apply the sigmoid activation function
-        output = ___
+        output = sigmoid(input_sum_with_bias)
 
         return output
 
